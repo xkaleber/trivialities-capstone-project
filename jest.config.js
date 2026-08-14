b2914c1986f23bfd25c22f34ff983addb1ad96d1
xkaleber <kberhane88@gmail.com>
2026-08-14 11:46:12 -0400

    Update jest.config.js
    - Adding <rootDir>/e2e/ to prevent Jest from attempting to read or execute files within that directory

--- jest.config.js
+++ jest.config.js
@@ -1,42 +1,22 @@
-import nextJest from 'next/jest.js';
-import { createRequire } from 'module';
+import nextJest from 'next/jest.js'; 
+import { createRequire } from 'module'; 
 
-const require = createRequire(import.meta.url);
+const require = createRequire(import.meta.url); 
+const createJestConfig = nextJest({ dir: './', }); 
 
-const createJestConfig = nextJest({
-  dir: './',
-});
-
-/** @type {import('jest').Config} */
-const config = {
-  coverageProvider: 'v8',
-  testEnvironment: 'node',
-  moduleNameMapper: {
-    '^@/(.*)$': '<rootDir>/src/$1',
-    '^bson$': require.resolve('bson'),
-  },
-};
+/** @type {import('jest').Config} */ 
+const config = { 
+  coverageProvider: 'v8', 
+  testEnvironment: 'node', 
+  // Adding this property to exclude Playwright end-to-end tests
+  testPathIgnorePatterns: [
+    '<rootDir>/node_modules/',
+    '<rootDir>/e2e/'
+  ],
+  moduleNameMapper: { 
+    '^@/(.*)$': '<rootDir>/src/$1', 
+    '^bson$': require.resolve('bson'), 
+  }, 
+}; 
 
 export default createJestConfig(config);
-
-// import nextJest from 'next/jest.js';
-// import { createRequire } from 'module';
-
-// const require = createRequire(import.meta.url);
-
-// const createJestConfig = nextJest({
-//   // Tells Jest where to load your next.config.js and .env settings
-//   dir: './',
-// });
-
-// /** @type {import('jest').Config} */
-// const config = {
-//   coverageProvider: 'v8',
-//   testEnvironment: 'node', // ✨ Changed from jsdom to node to support Mongoose
-//   moduleNameMapper: {
-//     '^@/(.*)$': '<rootDir>/src/$1',
-//     '^bson$': require.resolve('bson'),
-//   },
-// };
-
-// export default createJestConfig(config);
